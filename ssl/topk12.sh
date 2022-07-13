ed7fb73b03d1f3f3bef828a858f3f51681e2023b
Dan <danielmackie82@gmail.com>
2022-07-13 11:09:50 +0800

    improved topk12.sh

--- ssl/topk12.sh
+++ ssl/topk12.sh
@@ -2,6 +2,10 @@
 
 LBSHOME=~/projects/lychee/data/certbot/conf/live/littlebagshop.com
 
-openssl pkcs12 -export -in $LBSHOME/fullchain.pem -inkey $LBSHOME/privkey.pem -out springboot_letsencrypt.p12 -name bootalias -CAfile chain.pem -caname root
+if [[ -z "${OSSLPWD}" ]]; then
+   echo "environment variable OSSLPWD is not set" 
+else
+   openssl pkcs12 -export -in $LBSHOME/fullchain.pem -inkey $LBSHOME/privkey.pem -out springboot_letsencrypt.p12 -name bootalias -CAfile chain.pem -caname root -passout env:OSSLPWD
+cp springboot_letsencrypt.p12 ~/projects/sorbet/src/main/resources
+fi
 
-cp springboot_letsencrypt.p12 ../src/main/resources
