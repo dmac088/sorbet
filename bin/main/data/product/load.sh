7795caa90cae1af02cc0b659597ae7343581ab39
Dan <danielmackie82@gmail.com>
2022-07-13 16:10:58 +0800

    removed irrelevant from repo

--- bin/main/data/product/load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'using parameter' $1 
-
-curl -k --location --request POST 'https://localhost:8090/api/Product/Upload/' \
---header "authorization: Bearer $1" \
---header 'Content-Type: multipart/form-data' \
---form 'file=@product_master.tsv'
