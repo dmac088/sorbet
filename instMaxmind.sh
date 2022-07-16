8287ac5f4536d5d09c7254d5e62f5427eecf40ac
Daniel Mackie <danielmackie@Daniels-MacBook-Pro.local>
2022-07-16 15:12:57 +0800

    added maxmind database installer instMaxmind.sh

---
+++ instMaxmind.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export GEOIP_EDITION='GeoLite2-City'
+#export GEOIP_EDITION='GeoLite2-Country'
+export GEOIP_LICENCE_KEY=${MAXMIND_KEY}
+export GEOIP_TMP_FILE=geoip.tar.gz
+
+curl -sS "https://download.maxmind.com/app/geoip_download?edition_id=${GEOIP_EDITION}&license_key=${GEOIP_LICENCE_KEY}&suffix=tar.gz" > $GEOIP_TMP_FILE && \
+tar -xzf $GEOIP_TMP_FILE "${GEOIP_EDITION}*.mmdb"                                                                              && \
+mv ${GEOIP_EDITION}*/${GEOIP_EDITION}.mmdb ./src/main/resources/maxmind/"${GEOIP_EDITION}.mmdb"
+rm $GEOIP_TMP_FILE
