b4783941eb857a77c13e0401bedb502bb88ed9b0
Daniel Mackie <danielmackie@daniels-mbp.lan>
2022-08-04 14:17:18 +0800

    removal of unused imports

--- src/test/java/io/nzbee/integration/entity/IT_RefreshSchema.java
+++ src/test/java/io/nzbee/integration/entity/IT_RefreshSchema.java
@@ -1,9 +1,7 @@
 package io.nzbee.integration.entity;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-
 import javax.sql.DataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,7 +12,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.annotation.Rollback;
