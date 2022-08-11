298cb5961838a60d143c704a1540febe6b1f5a1e
Daniel Mackie <danielmackie@daniels-mbp.lan>
2022-08-11 12:00:14 +0800

    cleaned up discovery root object

--- src/main/java/io/nzbee/resources/discovery/DiscoveryResourceAssembler.java
+++ src/main/java/io/nzbee/resources/discovery/DiscoveryResourceAssembler.java
@@ -1,9 +1,7 @@
 package io.nzbee.resources.discovery;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
 import java.util.Map;
-
 import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Component;
 import io.nzbee.Constants;
@@ -21,27 +19,23 @@ public class DiscoveryResourceAssembler  implements ISimpleResourceAssembler<Dis
 
 	@Override
 	public DiscoveryResource toModel(Map<String, ?> m) {
+		//by passing in a map, we don't need to worry about a potentially unlimited number of parameters
+		
+		
+		//discovery root object
 		DiscoveryResource dr = new DiscoveryResource();
 		
-		//this one is a no brainer since categories drive the whole experience
-		dr.add(linkTo(methodOn(CategoryController.class).getProductCategories(null, null)).withRel("getAllCategories"));
-
-		dr.add(linkTo(methodOn(CustomerController.class).getCustomer(null)).withRel("getCustomer"));
+		//nouns / things / resources
+		dr.add(linkTo(methodOn(CategoryController.class).getProductCategories(null, null)).withRel("cateogries").expand(m));
+		dr.add(linkTo(methodOn(CustomerController.class).getCustomer(null)).withRel("customer").expand(m));
+		dr.add(linkTo(methodOn(BagController.class).getCustomerBag(null, null, null)).withRel("bag").expand(m));
+		dr.add(Link.of(Constants.tokenUrl).withRel("token"));
 		
-		dr.add(linkTo(methodOn(CustomerController.class).registerNewCustomer(null, null, null, null)).withRel("registerCustomer").expand(m));
-		dr.add(linkTo(methodOn(CustomerController.class).confirmRegistration(null, null, null)).withRel("confirmRegistration"));
-		dr.add(linkTo(methodOn(BagController.class).getCustomerBag(null, null, null)).withRel("getBag").expand(m));
-		
-		
-		//get product does not need to be here
-		//dr.add(linkTo(methodOn(ProductController.class).get(null, null, null)).withRel("getProduct"));
-		
-		
-		//not sure where these go, think they can stay in the discovery object
-		dr.add(linkTo(methodOn(SearchController.class).search(null, null, null, null, null, null, null, null)).withRel("searchProduct").expand(m));
-		dr.add(linkTo(methodOn(SearchController.class).getSuggestions(null, null, null)).withRel("searchSuggestion").expand(m));
-		
-		dr.add(Link.of(Constants.tokenUrl).withRel("accessTokens"));
+		//verbs / actions
+		dr.add(linkTo(methodOn(CustomerController.class).registerNewCustomer(null, null, null, null)).withRel("register").expand(m));
+		dr.add(linkTo(methodOn(CustomerController.class).confirmRegistration(null, null, null)).withRel("confirm").expand(m));
+		dr.add(linkTo(methodOn(SearchController.class).search(null, null, null, null, null, null, null, null)).withRel("search").expand(m));
+		dr.add(linkTo(methodOn(SearchController.class).getSuggestions(null, null, null)).withRel("suggest").expand(m));
 		
 		return dr;
 	}
