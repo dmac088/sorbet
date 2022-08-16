d06e6be1b884140b595fe2fe94fff7ac35dba81c
Daniel Mackie <danielmackie@daniels-mbp.lan>
2022-08-16 10:32:48 +0800

    removed redundant methog in BagResource Assembler

--- src/main/java/io/nzbee/resources/bag/BagResourceAssembler.java
+++ src/main/java/io/nzbee/resources/bag/BagResourceAssembler.java
@@ -16,20 +16,6 @@ public class BagResourceAssembler extends RepresentationModelAssemblerSupport<Ba
 		super(BagController.class, BagResource.class);
 	}
 
-	public BagResource toModel(BagView bag, String locale, String currency) {
-		BagResource br = new BagResource(bag);
-		br.add(linkTo(methodOn(ProductController.class).getShippingProviders(locale, currency)).withRel("getShippingProviders"));
-		br.add(linkTo(methodOn(ProductController.class).getShippingDestinations(locale)).withRel("getShippingDestinations"));
-		br.add(linkTo(methodOn(ProductController.class).getShippingTypes(null, null, null)).withRel("getShippingTypes"));
-		br.add(linkTo(methodOn(ProductController.class).getByDestinationAndType(locale, currency, null, null, null)).withRel("getShippingProduct"));
-		br.add(linkTo(methodOn(BagController.class).getCustomerBag(locale, currency, null)).withSelfRel());
-		br.add(linkTo(methodOn(BagController.class).getBagContents(locale, currency, null)).withRel("bagContents"));
-		br.add(linkTo(methodOn(BagController.class).addPhysicalItemToBag(locale, currency, null, null)).withRel("addItem"));
-		br.add(linkTo(methodOn(BagController.class).addShippingItemToBag(locale, currency, null, null)).withRel("addShipping"));
-		br.add(linkTo(methodOn(BagController.class).removeItemFromBag(locale, currency, null, null)).withRel("removeItem"));
-		return br;
-	}
-
 	@Override
 	public BagResource toModel(BagView bag) {
 		BagResource br = new BagResource(bag);
