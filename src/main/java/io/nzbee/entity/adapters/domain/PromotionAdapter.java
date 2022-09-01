b610abd336376b2900adbb4e0431f09efe20a7ed
Daniel Mackie <danielmackie@daniels-mbp.lan>
2022-09-01 22:47:55 +0800

    asdf

--- src/main/java/io/nzbee/entity/adapters/domain/PromotionAdapter.java
+++ src/main/java/io/nzbee/entity/adapters/domain/PromotionAdapter.java
@@ -1,13 +1,18 @@
 package io.nzbee.entity.adapters.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import io.nzbee.domain.bag.Bag;
 import io.nzbee.domain.ports.IPromotionPortService;
+import io.nzbee.domain.promotion.IBagPromotion;
 import io.nzbee.domain.promotion.Promotion;
 import io.nzbee.entity.promotion.IPromotionMapper;
+import io.nzbee.entity.promotion.PromotionDomainDTO;
 import io.nzbee.entity.promotion.type.IPromotionTypeService;
 import io.nzbee.entity.promotion.IPromotionDTOService;
 
@@ -41,9 +46,36 @@ public class PromotionAdapter implements IPromotionPortService {
 	
 		
 		//get tehbag promotions
-		System.out.println(promotionService.findBagPromotions().size());
-		System.out.println(promotionService.findShippingPromotion().size());
-		System.out.println(promotionService.findItemPromotion("15483827").size());
+//		System.out.println(promotionService.findBagPromotions().size());
+//		System.out.println(promotionService.findShippingPromotion().size());
+//		System.out.println(promotionService.findItemPromotion("15483827").size());
+		
+		//item level promotions
+		bag.getBagItems().forEach(i -> {
+			List<Promotion> lp = 
+					promotionService.findItemPromotion(i.getBagItem().getProductUPC())
+					.stream().map(p -> promotionMapper.DTOToDo(p))
+					.collect(Collectors.toList());
+			
+			lp.forEach(k -> {
+				IBagPromotion rbip = (IBagPromotion) k;
+				rbip.execute(i);
+			});
+		});
+		
+		//bag promotions
+		promotionService.findBagPromotions()
+		.forEach(dto -> {
+			//we need to check if the bag is eligible for the promotion before executing
+			//or this should be done within the execute method of the promotion object itself
+			
+			IBagPromotion p = (IBagPromotion)promotionMapper.DTOToDo(dto);
+			p.execute(bag);
+		});
+		
+		
+		
+		//shipping promotions
 		
 //		
 //		// for each promotion apply it
