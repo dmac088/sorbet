d6beae14d52dc3b6cf4d4ef0152056afa1b6d2a4
Daniel Mackie <danielmackie@daniels-mbp.lan>
2022-09-02 15:09:32 +0800

    removal of commented code which is redundant anyway

--- src/main/java/io/nzbee/entity/adapters/domain/PromotionAdapter.java
+++ src/main/java/io/nzbee/entity/adapters/domain/PromotionAdapter.java
@@ -76,66 +76,6 @@ public class PromotionAdapter implements IPromotionPortService {
 				});
 			}
 		});
-		
-		 
-//		//item level promotions
-//		bag.getBagItems().forEach(i -> {
-//			List<Promotion> lp = 
-//					promotionService.findItemPromotion(i.getBagItem().getProductUPC())
-//					.stream().map(p -> promotionMapper.DTOToDo(p))
-//					.collect(Collectors.toList());
-//			
-//			lp.forEach(k -> {
-//				IBagPromotion rbip = (IBagPromotion) k;
-//				rbip.execute(i);
-//			});
-//		});
-//				
-//		
-//		if(bag.hasShippingItem()) {
-//			promotionService.findShippingPromotion()
-//			.forEach(dto -> {
-//				//we need to check if the bag is eligible for the promotion before executing
-//				//or this should be done within the execute method of the promotion object itself
-//				IBagPromotion p = (IBagPromotion)promotionMapper.DTOToDo(dto);
-//				p.execute(bag.getShippingItem());
-//				
-//			});
-//		}
-		
-		//shipping promotions
-		
-//		
-//		// for each promotion apply it
-//		sp.forEach(p -> {
-//
-//			if (p.isBagType()) {
-//				Promotion pdo = promotionMapper.DTOToDo(p);
-//				IBagPromotion blp = (IBagPromotion) pdo;
-//				blp.execute(bag);
-//				return;
-//			}
-//
-//			if (p.isProductType()) {
-//				bag.getBagItems().stream().forEach(i -> {
-//					if (i.getBagItem().getProductUPC().equals(p.getPromotionUPC())) {
-//						Promotion pdo = promotionMapper.DTOToDo(p);
-//						IBagPromotion rbip = (IBagPromotion) pdo;
-//						rbip.execute(i);
-//						return;
-//					}
-//				});
-//			}
-//
-//			if (p.isShippingType()) {
-//				Promotion pdo = promotionMapper.DTOToDo(p);
-//				IBagPromotion sbip = (IBagPromotion) pdo;
-//				if (bag.hasShippingItem()) {
-//					sbip.execute(bag.getShippingItem());
-//				}
-//			}
-//
-//		});
 
 		return bag;
 	}
