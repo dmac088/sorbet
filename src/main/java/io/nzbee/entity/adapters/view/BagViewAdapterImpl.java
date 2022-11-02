c713d56d7fd3b3cb336d77d9fe5352207410656a
Daniel Mackie <danielmackie@daniels-mbp.lan>
2022-11-02 22:29:19 +0800

    removed unecessary if statement

--- src/main/java/io/nzbee/entity/adapters/view/BagViewAdapterImpl.java
+++ src/main/java/io/nzbee/entity/adapters/view/BagViewAdapterImpl.java
@@ -21,50 +21,44 @@ import io.nzbee.view.ports.IBagPortService;
 public class BagViewAdapterImpl implements IBagPortService {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
-	
+
 	@Autowired
 	private IBagViewDTOService bagService;
-	
+
 	@Autowired
 	private IBagViewDTOMapper bagMapper;
-	
+
 	@Autowired
 	private IHKPostService hkPostService;
-	
+
 	@Autowired
 	private IShippingProductService shippingProductService;
-	
+
 	@Override
 	@Transactional
 	public BagView findByCode(Locale locale, String userName) {
-		LOGGER.debug("call " + getClass().getSimpleName() + ".findByCode with parameter {}, {}, {}", locale, locale.getCurrency().getCurrencyCode(), userName);
-		
-		//this will also retrieve the items of the bag
-		Optional<BagViewDTO> ob = bagService.findByCode(locale.getLanguageCode(), locale.getCurrency().getCurrencyCode(),userName);
-		
-		
-		
-		if(ob.get().getBagItems().stream().filter(i -> i.getBagItemTypeCode().equals(Constants.shippingBagItemType)).findAny().isPresent()) {
-	
-			String shippingUpc = ob.get().getBagItems().stream().filter(i -> i.getBagItemTypeCode().equals(Constants.shippingBagItemType)).findAny().get().getBagItemUPC();
-			
-			//get the postage product details from the database
-			ShippingProductEntity s = shippingProductService.findByUpc(shippingUpc);
-			
-			//get the postage free from hk post
-			PostageProductViewDTO postageFee = hkPostService.getHKPostageFee(s.getShippingCountryCode(), s.getShippingCode(), ob.get().getTotalWeight());
-			
-			BigDecimal fee = (postageFee != null && postageFee.getTotalPostage() != null)
-								? postageFee.getTotalPostage()
-								: BigDecimal.ZERO;
-			
-			return bagMapper.toView(ob.get(), fee, locale);
-			
-		}
-		return bagMapper.toView(ob.get());
+		LOGGER.debug("call " + getClass().getSimpleName() + ".findByCode with parameter {}, {}, {}", locale,
+				locale.getCurrency().getCurrencyCode(), userName);
+
+		// this will also retrieve the items of the bag
+		Optional<BagViewDTO> ob = bagService.findByCode(locale.getLanguageCode(),
+				locale.getCurrency().getCurrencyCode(), userName);
+
+		String shippingUpc = ob.get().getBagItems().stream()
+				.filter(i -> i.getBagItemTypeCode().equals(Constants.shippingBagItemType)).findAny().get()
+				.getBagItemUPC();
+
+		// get the postage product details from the database
+		ShippingProductEntity s = shippingProductService.findByUpc(shippingUpc);
+
+		// get the postage free from hk post
+		PostageProductViewDTO postageFee = hkPostService.getHKPostageFee(s.getShippingCountryCode(),
+				s.getShippingCode(), ob.get().getTotalWeight());
+
+		BigDecimal fee = (postageFee != null && postageFee.getTotalPostage() != null) ? postageFee.getTotalPostage()
+				: BigDecimal.ZERO;
+
+		return bagMapper.toView(ob.get(), fee, locale);
 	}
-	
-	
-	
 
 }
